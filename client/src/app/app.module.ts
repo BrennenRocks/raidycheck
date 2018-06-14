a941aa10042bb1956908f989dbbe64569db6fbb1
bdavis <brennen.davis@fishbowlinventory.com>
2018-06-14 13:24:46 -0600

    Install Toastr and start navbar component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    ToastrModule.forRoot({
+      closeButton: true,
+      timeOut: 5000,
+      preventDuplicates: true,
+      easing: 'ease-in'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
