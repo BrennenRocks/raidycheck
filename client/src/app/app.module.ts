57a8bfa3d1121a6055fd4f666ccddde32a95ef4b
Brennen Davis <brennendavis@gmail.com>
2018-06-14 22:43:14 -0600

    Routing and Home Component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { ToastrModule } from 'ngx-toastr';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MDBBootstrapModule.forRoot(),
     ToastrModule.forRoot({
       closeButton: true,
