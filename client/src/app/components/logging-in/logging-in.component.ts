1be7b224b0177e817fa3dfd974f84cc6953ea70b
Brennen Davis <brennendavis@gmail.com>
2018-06-26 21:26:58 -0600

    Elements in Sidebar disappear properly

--- client/src/app/components/logging-in/logging-in.component.ts
+++ client/src/app/components/logging-in/logging-in.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { GroupsService } from '../../services/groups.service';
+import { ServerResponse } from '../../interfaces/server-response';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-logging-in',
@@ -11,13 +14,22 @@ export class LoggingInComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
+    private groupsService: GroupsService,
+    private toastr: ToastrService,
     private route: ActivatedRoute,
     private router: Router
   ) { }
 
   ngOnInit() {
-      this.authService.storeToken(this.route.snapshot.queryParams.rc_token);
-      this.route.snapshot.queryParams.new === 'true' ? this.router.navigate(['/get-started']) : this.router.navigate(['/groups']);
+    this.authService.storeToken(this.route.snapshot.queryParams.rc_token);
+    this.groupsService.getGroups().subscribe((data: ServerResponse) => {
+      if (!data.success) {
+        this.router.navigate(['/']);
+        this.toastr.error(data.message, 'Error');
+      } else {
+        this.route.snapshot.queryParams.new === 'true' ? this.router.navigate(['/get-started']) : this.router.navigate(['/group', data.group._id]);
+      }
+    });   
   }
 
 }
